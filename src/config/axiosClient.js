036bfa46adce57ee2c64b4da44e85e7b02d8d671
Josue07210 <josue071002@gmail.com>
2025-11-13 16:12:20 -0500

    Agregue carpeta config al src y en la carpeta config esta el axiosClient que se encarga de conectar el frontend con el backend de mi sv

---
+++ src/config/axiosClient.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+// La URL donde corre mi sv de express
+const API_BASE_URL = 'http://localhost:3000/api'; 
+
+const axiosClient = axios.create({
+    baseURL: API_BASE_URL
+});
+
+export default axiosClient;
